a1c161d9a8e4e9646e297dddca38602b47b33430
Conor Knowles <ck@conorknowles.com>
2022-01-02 18:18:02 +0000

    cursor pos autocmd

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,19 +1,21 @@
-local utils = require('utils')
-
+local utils = require("utils")
 -- relative line numbers
-utils.opt('o', 'number', true)
+utils.opt("o", "number", true)
 local autocmds = {
-    --- Current window has hybrid numbers
-    --- All other windows have absolute numbers
-    numberToggle = {
-        {"BufEnter,FocusGained,InsertLeave", "*", "set relativenumber"},
-        {"BufLeave,FocusLost,InsertEnter", "*", "set norelativenumber"}
-    },
-    spellcheck = {
-      {"FileType", "text,markdown", "setlocal spell"}
-    },
-    disableAutoComment = {
-      {"FileType", "*", "setlocal formatoptions-=c formatoptions-=r formatoptions-=o"}
-    },
+	--- Current window has hybrid numbers
+	--- All other windows have absolute numbers
+	number_toggle = {
+		{ "BufEnter,FocusGained,InsertLeave", "*", "set relativenumber" },
+		{ "BufLeave,FocusLost,InsertEnter", "*", "set norelativenumber" },
+	},
+	spellcheck = {
+		{ "FileType", "text,markdown", "setlocal spell" },
+	},
+	disableAutoComment = {
+		{ "FileType", "*", "setlocal formatoptions-=c formatoptions-=r formatoptions-=o" },
+	},
+	restore_cursor = {
+		{ "BufRead", "*", [[call setpos(".", getpos("'\""))]] },
+	},
 }
 utils.augroups(autocmds)
