002c3172e7e2ae4392910bf86fd4080f92cef515
Conor Knowles <ck@conorknowles.com>
2022-01-02 18:05:41 +0000

    removed legacy code, added new formatter for java

--- lua/plugin_settings/formatter.lua
+++ lua/plugin_settings/formatter.lua
@@ -1,37 +1,48 @@
 local present, formatter = pcall(require, "formatter")
 if not present then
-   return
+	return
 end
 
 formatter.setup({
-filetype = {
-    c = {
-       function()
-          return {
-            exe = "clang-format",
-            args = {"--assume-filename", vim.api.nvim_buf_get_name(0)},
-            stdin = true,
-            cwd = vim.fn.expand('%:p:h')  -- Run clang-format in cwd of the file.
-          }
-        end
-    },
-    lua = {
-      function()
-        return {
-          exe = "stylua",
-          stdin = true,
-        }
-      end,
-      },
-    python = {
-      -- Configuration for psf/black
-      function()
-        return {
-          exe = "black", -- this should be available on your $PATH
-          args = { '-' },
-          stdin = true,
-        }
-      end
-    }
-  }
+	filetype = {
+		c = {
+			function()
+				return {
+					exe = "clang-format",
+					args = { "--assume-filename", vim.api.nvim_buf_get_name(0) },
+					stdin = true,
+					cwd = vim.fn.expand("%:p:h"),  -- Run clang-format in cwd of the file.
+				}
+			end,
+		},
+		lua = {
+			function()
+				return {
+					exe = "stylua",
+					args = { "-" },
+					stdin = true,
+				}
+			end,
+		},
+		python = {
+			-- Configuration for psf/black
+			function()
+				return {
+					exe = "black", -- this should be available on your $PATH
+					args = { "-" },
+					stdin = true,
+				}
+			end,
+		},
+		java = {
+			function()
+				return {
+					exe = "gjf",
+					-- Formatter uses '-' as stdin
+					args = { "-" },
+					stdin = true,
+				}
+			end,
+		},
+	},
 })
