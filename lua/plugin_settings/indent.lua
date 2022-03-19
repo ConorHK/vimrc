452735333c8c7fa527dbf42f117c2be74b114214
ConorHK <dev@conorknowles.com>
2022-03-19 23:17:35 +0000

    feat: Add treesitter enabled indent

--- lua/plugin_settings/indent.lua
+++ lua/plugin_settings/indent.lua
@@ -3,18 +3,8 @@ if not present then
 	return
 end
 
-vim.cmd([[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]])
+vim.opt.list = true
 
 indent.setup({
-	char = "",
-	char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-	},
-	space_char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-	},
-	show_trailing_blankline_indent = false,
+  show_current_context = true,
 })
