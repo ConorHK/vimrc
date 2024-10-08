97d8051ab13c01a7420def264503ccb4f14d98c7
Conor Knowles <knoconor@amazon.com>
2024-10-08 18:17:46 +0000

    Move config from plugin to file

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,4 +1,19 @@
 local M = {}
 function M.setup()
+	local present, kanagawa = pcall(require, "kanagawa")
+	if not present then
+		return
+	end
+	require("kanagawa").setup({
+		colors = {
+			theme = {
+				all = {
+					ui = {
+						bg_gutter = "none"
+					}
+				}
+			}
+		}
+	})
 end
 return M
--- lua/config/movement.lua
+++ lua/config/movement.lua
@@ -16,6 +16,12 @@ function M.setup()
 		}
 	})
 
+	vim.keymap.set({"n", "x", "o"}, "s", function() require("flash").jump() end, { desc = "Flash" })
+	vim.keymap.set({"n", "x", "o"}, "S", function() require("flash").treesitter() end, { desc = "Flash treesitter" })
+	vim.keymap.set("o", "r", function() require("flash").remote() end, { desc = "Remote flash" })
+	vim.keymap.set({"x", "o"}, "R", function() require("flash").treesitter_search() end, { desc = "Flash treesitter search" })
+	vim.keymap.set("c", "<c-s>", function() require("flash").toggle() end, { desc = "Toggle flash" })
+
 end
 
 return M
