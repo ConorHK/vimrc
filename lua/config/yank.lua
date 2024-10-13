29524990653bff868d918f3c369c7d6439f14151
Conor Knowles <github@conorknowles.com>
2024-10-13 15:33:26 +0000

    Enable smartyank

--- lua/config/yank.lua
+++ lua/config/yank.lua
@@ -1,8 +1,9 @@
 local M = {}
 function M.setup()
-	local present, _ = pcall(require, "smartyank")
+	local present, smartyank = pcall(require, "smartyank")
 	if not present then
 		return
 	end
+	smartyank.setup({})
 end
 return M
