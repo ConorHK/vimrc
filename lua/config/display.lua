d0ba8f4b6944e95241eba09bd11251df0a2a46dd
Conor Knowles <dev@conorknowles.com>
2024-10-31 09:41:40 +0000

    Fix macro notification

--- lua/config/display.lua
+++ lua/config/display.lua
@@ -26,6 +26,12 @@ function M.setup()
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
 			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
+		routes = {
+			{
+				view = "notify",
+				filter = { event = "msg_showmode" },
+			},
+		},
 	})
 end
 
