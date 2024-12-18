839f25e7f1e2173e0c7bf20c34b0e523988f7cad
Conor Knowles <dev@conorknowles.com>
2024-12-18 00:18:28 +0000

    Update telescope setup

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,68 +1,68 @@
 local M = {}
 
 function M.setup()
-	local present, telescope = pcall(require, "telescope")
-	if not present then
-		return
-	end
-
 	local map = vim.keymap.set
 	local default_opts = { noremap = true, silent = true }
+	local telescope = require("telescope")
+	local pickers = require("telescope.pickers")
 	local actions = require("telescope.actions")
+	local finders = require("telescope.finders")
+	local make_entry = require("telescope.make_entry")
+	local conf = require("telescope.config").values
+	local sorters = require("telescope.sorters")
+	local themes = require("telescope.themes")
 
-	telescope.load_extension("smart_open")
-	map("n", "<leader>t", "<cmd>Telescope smart_open theme=ivy<CR>", default_opts)
-	map("n", "<leader>g", "<cmd>lua require('telescope.builtin').live_grep()<CR>", default_opts)
+	local live_multigrep = function(opts)
+			opts = opts or {}
+			opts.cwd = opts.cwd or vim.uv.cwd()
+			local finder = finders.new_async_job {
+					command_generator = function(prompt)
+							if not prompt or prompt == "" then
+									return nil
+							end
+							local pieces = vim.split(prompt, "  ")
+							local args = { "rg" }
+							if pieces[1] then
+									table.insert(args, "-e")
+									table.insert(args, pieces[1])
+							end
+							if pieces[2] then
+									table.insert(args, "-g")
+									table.insert(args, pieces[2])
+							end
+
+							---@diagnostic disable-next-line deprecated
+							return vim.tbl_flatten {
+									args,
+									{ "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+							}
+					end,
+					entry_maker = make_entry.gen_from_vimgrep(opts),
+					cwd = opts.cwd,
+			}
+			pickers.new(opts, {
+					debounce = 100,
+					prompt_title = "Multigrep",
+					theme = themes.ivy,
+					finder = finder,
+					previewer = conf.grep_previewer(opts),
+					sorter = sorters.empty(),
+			}):find()
+	end
+
+	map("n", "<leader>t", "<cmd>Telescope find_files<CR>", default_opts)
+	map("n", "<leader>g", live_multigrep, default_opts)
 	map("n", "<leader>u", "<cmd>Telescope undo<CR>", default_opts)
 
 	telescope.load_extension("harpoon")
 	telescope.load_extension("undo")
 	telescope.setup({
 		defaults = {
-			vimgrep_arguments = {
-				"rg",
-				"--color=never",
-				"--no-heading",
-				"--with-filename",
-				"--line-number",
-				"--column",
-				"--smart-case",
-			},
 			prompt_prefix = "➜  ",
 			selection_caret = " > ",
 			entry_prefix = "  ",
 			initial_mode = "insert",
-			selection_strategy = "reset",
-			sorting_strategy = "ascending",
-			layout_strategy = "horizontal",
-			layout_config = {
-				horizontal = {
-					prompt_position = "top",
-					preview_width = 0.55,
-					results_width = 0.8,
-				},
-				vertical = {
-					mirror = false,
-				},
-				width = 0.87,
-				height = 0.80,
-				preview_cutoff = 120,
-			},
-			file_sorter = require("telescope.sorters").get_fuzzy_file,
-			file_ignore_patterns = {},
-			generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-			path_display = { "absolute" },
-			winblend = 0,
-			border = {},
-			borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
 			color_devicons = true,
-			use_less = true,
-			set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-			file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-			grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-			qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-			-- Developer configurations: Not meant for general override
-			buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
 			mappings = {
 				i = {
 					["<esc>"] = actions.close,
@@ -71,9 +71,6 @@ function M.setup()
 		},
 		pickers = {
 			find_files = {
-				theme = "ivy",
-			},
-			live_grep = {
 				theme = "ivy",
 			},
 			harpoon = {
@@ -86,9 +83,6 @@ function M.setup()
 				override_generic_sorter = true, -- override the generic sorter
 				override_file_sorter = true, -- override the file sorter
 				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-			},
-			smart_open = {
-				match_algorithm = "fzf",
 			},
 		},
 	})
