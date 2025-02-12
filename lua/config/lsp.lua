548de0342886bc6f826fbce1e80ed4095b4369f2
Conor Knowles <knoconor@amazon.com>
2025-02-12 12:25:16 +0000

    Fix ruff, disable some pyright features

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -49,25 +49,25 @@ function M.setup()
 			},
 		},
 		pyright = {
-			-- settings = {
-			-- 	pyright = {
-			-- 		disableOrganizeImports = true,
-			-- 	},
-			-- 	python = {
-			-- 		analysis = {
-			-- 			ignore = { "*" },
-			-- 			typeCheckingMode = "off",
-			-- 			autoSearchPaths = true,
-			-- 			useLibraryCodeForTypes = true,
-			-- 		},
-			-- 		disableLanguageServices = false,
-			-- 	},
-			-- },
+			settings = {
+				pyright = {
+					disableOrganizeImports = true,
+				},
+				python = {
+					analysis = {
+						ignore = { "*" },
+						typeCheckingMode = "off",
+						autoSearchPaths = true,
+						useLibraryCodeForTypes = true,
+					},
+					disableLanguageServices = false,
+				},
+			},
 		},
 		ruff = {
 			init_options = {
 				settings = {
-					args = { "--config=" .. ruff_file },
+					configuration = ruff_file,
 				},
 			},
 			commands = {
@@ -231,6 +231,20 @@ function M.setup()
 				end
 			end
 		end,
+	})
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+		callback = function(args)
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			if client == nil then
+				return
+			end
+			if client.name == 'ruff' then
+				-- Disable hover in favor of Pyright
+				client.server_capabilities.hoverProvider = false
+			end
+		end,
+		desc = 'LSP: Disable hover capability from Ruff',
 	})
 
 	vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
