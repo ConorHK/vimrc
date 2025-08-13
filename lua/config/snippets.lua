c0ec7356c3bd9dabcd94ea4bb94f07fe0e8794ba
Conor Knowles <knoconor@amazon.com>
2025-08-13 12:47:58 +0000

    Revert "Remove debugger"
    This reverts commit 74d6b716b0a84ac1263cd9afed7ef78a1a22749a.

--- lua/config/snippets.lua
+++ lua/config/snippets.lua
@@ -17,7 +17,19 @@ function M.setup()
 	})
 
 	local snippets_path = vim.fn.stdpath("config") .. "/snippets/"
+	print("Loading snippets from:", snippets_path)
+	print("Path exists:", vim.fn.isdirectory(snippets_path))
+
 	require("luasnip.loaders.from_lua").load({ paths = snippets_path })
+
+	-- Debug: print loaded snippets
+	vim.defer_fn(function()
+		local snippets = ls.get_snippets("python")
+		print("Loaded python snippets:", #snippets)
+		for _, snip in ipairs(snippets) do
+			print("  - " .. snip.trigger)
+		end
+	end, 100)
 
 	vim.keymap.set("i", "<C-e>", function()
 		if ls.expand_or_jumpable() then
