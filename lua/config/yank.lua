4ba9f05578889a433bf8aeb52f3f3e950f8ae33d
Conor Knowles <knoconor@amazon.com>
2026-03-04 14:58:53 +0000

    Move copy to nvim

--- lua/config/yank.lua
+++ lua/config/yank.lua
@@ -12,11 +12,16 @@ function M.setup()
     vim.keymap.set("n", "<leader>cp", ":CopyFilePath<CR>", { silent = true, noremap = true })
     vim.keymap.set("n", "<leader>cu", function()
         local file = vim.fn.expand("%:p")
+        local dir = vim.fn.fnamemodify(file, ":h")
         local line = vim.fn.line(".")
-        local result = vim.fn.system({ "git-url", file, tostring(line) })
+        local result = vim.trim(vim.fn.system({ "git", "-C", dir, "url", file, tostring(line) }))
         if vim.v.shell_error ~= 0 then
             vim.notify("git-url: " .. result, vim.log.levels.ERROR)
+            return
         end
+        local b64 = vim.base64.encode(result)
+        io.stdout:write(string.format("\027]52;c;%s\a", b64))
+        print(result)
     end, { silent = true, noremap = true })
 end
 return M
